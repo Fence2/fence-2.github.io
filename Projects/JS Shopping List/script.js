ad9746c8283b5eb8f1115006efcd4f8dad8c1400
Dmitrii Chistyakov <chist.dmitrii@gmail.com>
2020-08-02 15:42:19 +0500

    uploading project "JS Shopping List"

---
+++ Projects/JS Shopping List/script.js
@@ -0,0 +1,36 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createrListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createrListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createrListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+ 
+
+// var button = document.getElementById("button1");
+// button.addEventListener("click", function() {
+// 	console.log("ХЕЙ");
+// });
