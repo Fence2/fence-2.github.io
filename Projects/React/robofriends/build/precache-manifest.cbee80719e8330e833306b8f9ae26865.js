08887ba1de4bc3a88f3cf3fc1dfcc6e9f4922642
Dmitrii Chistyakov <chist.dmitrii@gmail.com>
2020-08-14 18:33:43 +0500

    finished the Robofriends project

---
+++ Projects/React/robofriends/build/precache-manifest.cbee80719e8330e833306b8f9ae26865.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398ab70e3f3c43a1d07976284f645227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bf25ec149d1571e2448a",
+    "url": "/static/css/main.dce762fd.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf25ec149d1571e2448a",
+    "url": "/static/js/main.d38c645f.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
